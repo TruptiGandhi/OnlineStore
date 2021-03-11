99633d7381a3b59298f08e0681a41b5313f68cae
TruptiGandhi <59397004+TruptiGandhi@users.noreply.github.com>
2021-03-11 13:45:16 +0530

    commit2

--- ECommerceProject/Controllers/StoresController.cs
+++ ECommerceProject/Controllers/StoresController.cs
@@ -19,6 +19,19 @@ namespace ECommerceProject.Controllers
             var stores = db.Stores.Include(s => s.UserDetails);
             return View(stores.ToList());
         }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
 
         public ActionResult Create()
         {
@@ -39,6 +52,66 @@ namespace ECommerceProject.Controllers
 
             ViewBag.User_Id = new SelectList(db.UserDetails, "User_id", "FirstName", store.User_Id);
             return View(store);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.User_Id = new SelectList(db.UserDetails, "User_id", "FirstName", store.User_Id);
+            return View(store);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StoreId,StoreName,URL,Address,LogoURL,BusinessCategory,User_Id")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(store).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.User_Id = new SelectList(db.UserDetails, "User_id", "FirstName", store.User_Id);
+            return View(store);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Store store = db.Stores.Find(id);
+            db.Stores.Remove(store);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
     }
